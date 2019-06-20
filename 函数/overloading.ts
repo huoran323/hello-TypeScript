10d3c84fb78f08f4b0450b061bab540ac47c29e6
霍然 <303176152@qq.com>
2019-06-20 17:27:37 +0800

    submit

--- 函数/overloading.ts
+++ 函数/overloading.ts
@@ -18,3 +18,41 @@ console.log(n);
 
 n = sum(1, 2);
 console.log(n);
+
+function divide(x: number, y: number): number;
+function divide(str: string, y: number): string[];
+
+function divide(x: any, y: number): any {
+  if (typeof x === "number") {
+    return x / y;
+  } else if (typeof x === "string") {
+    return [x.substring(0, y), x.substring(y)];
+  }
+}
+
+let n: number = divide(6, 2);
+console.log(n);
+
+let s: string[] = divide("football", 3);
+console.log(s);
+
+// 静态方法和实例方法
+class Util {
+  static divide(x: number, y: number): number;
+  static divide(str: string, y: number): string[];
+
+  static divide(x: any, y: number): any {
+    if (typeof x === "number") {
+      return x / y;
+    } else if (typeof x === "string") {
+      return [x.substring(0, y), x.substring(y)];
+    }
+  }
+}
+
+// let a: Util = new Util();
+// console.log(a.divide(6, 2));
+// console.log(a.divide('hello world', 4));
+
+let c: number = Util.divide(6, 2);
+console.log(c);
